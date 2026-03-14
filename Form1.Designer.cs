654f007a5d67332318cd1f1ea08097ce5060c9df
Delano <del.191006@gmail.com>
2026-03-14 14:08:24 +0700

    Menambahkan Tampilan GUI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            btnConvert = new Button();
+            label3 = new Label();
+            txtNilaiAwal = new TextBox();
+            txtNilaiAkhir = new TextBox();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Celcius", "Fahrenheit", "Kelvin", "Reamur" });
+            comboBox1.Location = new Point(69, 132);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 28);
+            comboBox1.TabIndex = 0;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(69, 211);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(151, 28);
+            comboBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(69, 89);
+            label1.Name = "label1";
+            label1.Size = new Size(91, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Satuan Awal";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(266, 89);
+            label2.Name = "label2";
+            label2.Size = new Size(77, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Nilai Awal";
+            label2.Click += label2_Click;
+            // 
+            // btnConvert
+            // 
+            btnConvert.Location = new Point(466, 132);
+            btnConvert.Name = "btnConvert";
+            btnConvert.Size = new Size(94, 29);
+            btnConvert.TabIndex = 4;
+            btnConvert.Text = "Convert";
+            btnConvert.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(69, 188);
+            label3.Name = "label3";
+            label3.Size = new Size(92, 20);
+            label3.TabIndex = 5;
+            label3.Text = "Satuan Akhir";
+            // 
+            // txtNilaiAwal
+            // 
+            txtNilaiAwal.Location = new Point(266, 132);
+            txtNilaiAwal.Name = "txtNilaiAwal";
+            txtNilaiAwal.Size = new Size(125, 27);
+            txtNilaiAwal.TabIndex = 6;
+            // 
+            // txtNilaiAkhir
+            // 
+            txtNilaiAkhir.Location = new Point(266, 212);
+            txtNilaiAkhir.Name = "txtNilaiAkhir";
+            txtNilaiAkhir.Size = new Size(125, 27);
+            txtNilaiAkhir.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(txtNilaiAkhir);
+            Controls.Add(txtNilaiAwal);
+            Controls.Add(label3);
+            Controls.Add(btnConvert);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private Label label1;
+        private Label label2;
+        private Button btnConvert;
+        private Label label3;
+        private TextBox txtNilaiAwal;
+        private TextBox txtNilaiAkhir;
     }
 }
--- obj/Debug/net10.0-windows/Modul3_103022400045.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/Modul3_103022400045.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Modul3_103022400045")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fe40789b3e0d45ad5fe980a7aa71f7db17726b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Modul3_103022400045")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Modul3_103022400045")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
